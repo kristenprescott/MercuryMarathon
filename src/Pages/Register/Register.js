b18da5bd10f37952af4d75bbdb07970e2bea9c59
Kristen Prescott <prescottdarling@gmail.com>
2021-04-26 13:18:43 -0400

    goal_time formatted properly, working on sending that data correctly on submit

--- src/Pages/Register/Register.js
+++ src/Pages/Register/Register.js
@@ -2,19 +2,24 @@ import { useState, useEffect } from "react";
 import "../Register/register.scss";
 
 export default function Register() {
+  const [goalTime, setGoalTime] = useState({
+    hours: "00",
+    minutes: "00",
+    seconds: "00",
+  });
   const [runners, setRunners] = useState([]);
   const [newRunner, setNewRunner] = useState({
-    id: "",
-    firstName: "",
-    lastName: "",
+    // id: "",
+    first_name: "",
+    last_name: "",
     sex: "",
     email: "",
     dob: "",
     city: "",
     state: "",
-    country: "",
-    goalTime: "",
-    time: "",
+    // cohort: "",
+    // real_time: "",
+    goal_time: "",
     wheelchair: false,
   });
 
@@ -22,89 +27,137 @@ export default function Register() {
     (async () => {
       try {
         const res = await fetch(
-          "https://next.json-generator.com/api/json/get/NJNIgRqLc"
+          "https://mercury-marathon-api.herokuapp.com/runner/"
         );
         const data = await res.json();
         console.log(data);
-        setRunners([data]);
+        setRunners([...data]);
       } catch (error) {
         console.error(error);
       }
     })();
     //
-    (async () => {
-      try {
-        const res = await fetch(
-          "https://next.json-generator.com/api/json/get/EkNmLRiLq"
-        );
-        const data = await res.json();
-        console.log(data);
-        setNewRunner([...runners, data]);
-      } catch (error) {
-        console.error(error);
-      }
-    })();
+    // (async () => {
+    //   try {
+    //     const res = await fetch(
+    //       "https://mercury-marathon-api.herokuapp.com/runner/"
+    //     );
+    //     const data = await res.json();
+    //     console.log(data);
+    //     setNewRunner([...runners, data]);
+    //   } catch (error) {
+    //     console.error(error);
+    //   }
+    // })();
   }, []);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    try {
-      const response = await fetch(
-        "https://next.json-generator.com/api/json/get/EkNmLRiLq",
-        {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify(newRunner),
-        }
-      );
-      const data = await response.json();
-      setRunners([...runners, data]);
-      // clear input after submission
-      setNewRunner({
-        first_name: "",
-        last_name: "",
-        sex: "",
-        email: "",
-        dob: "",
-        city: "",
-        state: "",
-        country: "",
-        goalTime: "",
-        wheelchair: false,
-      });
-    } catch (error) {
-      console.error(error);
-    }
+    console.log(JSON.stringify(newRunner));
+    // try {
+    //   const res = await fetch(
+    //     "https://mercury-marathon-api.herokuapp.com/runner/",
+    //     {
+    //       method: "POST",
+    //       headers: {
+    //         "Content-Type": "application/json",
+    //       },
+    //       body: JSON.stringify(newRunner),
+    //     }
+    //   );
+    //   const data = await res.json();
+    //   setRunners([...runners, data]);
+    //   // clear input after submission
+    //   setNewRunner({
+    //     first_name: "",
+    //     last_name: "",
+    //     sex: "",
+    //     email: "",
+    //     dob: "",
+    //     city: "",
+    //     state: "",
+    //     // cohort: "",
+    //     real_time: "",
+    //     goal_time: "",
+    //     wheelchair: false,
+    //   });
+    // } catch (error) {
+    //   console.error(error);
+    // }
   };
 
   const handleChange = (e) => {
+    console.log(e.target.dataset);
+
+    // if (e.target.dataset.type === "goal_time") {
+    //   console.log("goal_time");
+    // }
+
     setNewRunner({ ...newRunner, [e.target.name]: e.target.value });
   };
+
+  const handleGoalTimeChange = (e) => {
+    // debugger;
+    let value = e.target.value;
+    if (parseInt(value) < 10) {
+      value = "0" + value;
+    }
+    let time;
+    if (e.target.name === "hours") {
+      time = `${value}:${goalTime.minutes}:${goalTime.seconds}`;
+    } else if (e.target.name === "minutes") {
+      time = `${goalTime.hours}:${value}:${goalTime.seconds}`;
+    } else if (e.target.value === "seconds") {
+      time = `${goalTime.hours}:${goalTime.minutes}:${value}`;
+    }
+    setGoalTime({ ...goalTime, [e.target.name]: value });
+    setNewRunner({ ...newRunner, goal_time: time });
+  };
+
+  // const formatTime = (timeUnit) => {
+  //   //  "goal_time": "00:04:30",
+  //   if (timeUnit === "hours") {
+  //   }
+  // }
 
   return (
     <div className="Register container">
       <h1>Register for Mercury 2022</h1>
+      {/* 
+      <div>
+        {runners.map((runner, idx) => (
+          <h1 key={runner.id}>
+            <span>{runner.first_name}</span> <span>{runner.last_name}</span>{" "}
+            <br />
+            <span>{}</span>
+            <button
+              onClick={(e) => {
+                handleDelete(e, runner.id, idx);
+              }}
+            >
+              DELETE ME
+            </button>
+          </h1>
+        ))}
+      </div> */}
 
       <form onSubmit={handleSubmit}>
         {/* NAME INPUT */}
         <div className="container">
-          <label htmlFor="firstname">First Name:</label>
-
+          <label htmlFor="first_name">First Name:</label>
           <input
-            required
-            id="firstname"
+            // required
+            // id="first_name"
             type="text"
             name="first_name"
             value={newRunner.first_name}
             onChange={handleChange}
           />
-          <label htmlFor="lastname">Last Name:</label>
+          <label htmlFor="last_name">Last Name:</label>
           <input
-            required
-            id="lastname"
+            // required
+            // id="last_name"
             type="text"
             name="last_name"
             value={newRunner.last_name}
@@ -125,7 +178,7 @@ export default function Register() {
             name="dob"
             min="1900-01-01"
             max="2005-01-01"
-            id={newRunner.id}
+            // id={newRunner.id}
             onChange={handleChange}
             value={newRunner.DOB}
           ></input>
@@ -139,8 +192,10 @@ export default function Register() {
           <div className="select">
             <select required id="sex" name="sex" onChange={handleChange}>
               <option value="">--Select sex--</option>
-              <option value={newRunner.sex}>Female</option>
-              <option value={newRunner.sex}>Male</option>
+              {/* <option value={newRunner.sex}>Female</option>
+              <option value={newRunner.sex}>Male</option> */}
+              <option value="F">Female</option>
+              <option value="M">Male</option>
             </select>
           </div>
         </div>
@@ -153,7 +208,8 @@ export default function Register() {
           <input
             required
             id="email"
-            type="email"
+            // type="email"
+            type="text"
             name="email"
             value={newRunner.email}
             onChange={handleChange}
@@ -167,8 +223,8 @@ export default function Register() {
           <div className="wrapper">
             <label htmlFor="city">City:</label>
             <input
-              required
-              id="city"
+              // required
+              // id="city"
               type="text"
               name="city"
               value={newRunner.city}
@@ -179,23 +235,11 @@ export default function Register() {
           <div className="wrapper">
             <label htmlFor="state">State:</label>
             <input
-              required
-              id="state"
+              // required
+              // id="state"
               type="text"
               name="state"
               value={newRunner.state}
-              onChange={handleChange}
-            />
-          </div>
-
-          <div className="wrapper">
-            <label htmlFor="country">Country:</label>
-            <input
-              required
-              id="country"
-              type="text"
-              name="country"
-              value={newRunner.country}
               onChange={handleChange}
             />
           </div>
@@ -204,96 +248,98 @@ export default function Register() {
         <hr></hr>
 
         {/* COHORT SELECT */}
-        <div className="container">
+        {/* <div className="container">
           <label htmlFor="cohort">Cohort:</label>
           <div className="select">
             <select required id="cohort" name="cohort" onChange={handleChange}>
               <option value="">--Select cohort--</option>
               <optgroup label="Male">
-                <option value={newRunner.cohort}>14 and under</option>
-                <option value={newRunner.cohort}>15-19</option>
-                <option value={newRunner.cohort}>20-24</option>
-                <option value={newRunner.cohort}>25-29</option>
-                <option value={newRunner.cohort}>30-34</option>
-                <option value={newRunner.cohort}>35-39</option>
-                <option value={newRunner.cohort}>40-44</option>
-                <option value={newRunner.cohort}>45-49</option>
-                <option value={newRunner.cohort}>50-54</option>
-                <option value={newRunner.cohort}>55-59</option>
-                <option value={newRunner.cohort}>60-64</option>
-                <option value={newRunner.cohort}>70-74</option>
-                <option value={newRunner.cohort}>75-79</option>
-                <option value={newRunner.cohort}>80-84</option>
-                <option value={newRunner.cohort}>85-89</option>
-                <option value={newRunner.cohort}>90+</option>
+                <option value="M18-39">18-39</option>
+                <option value="M40-49">40-49</option>
+                <option value="M50-59">50-59</option>
+                <option value="M60-69">60-69</option>
+                <option value="M70+">70+</option>
               </optgroup>
               <optgroup label="Female">
-                <option>14 and under</option>
-                <option value={newRunner.cohort}>14 and under</option>
-                <option value={newRunner.cohort}>15-19</option>
-                <option value={newRunner.cohort}>20-24</option>
-                <option value={newRunner.cohort}>25-29</option>
-                <option value={newRunner.cohort}>30-34</option>
-                <option value={newRunner.cohort}>35-39</option>
-                <option value={newRunner.cohort}>40-44</option>
-                <option value={newRunner.cohort}>45-49</option>
-                <option value={newRunner.cohort}>50-54</option>
-                <option value={newRunner.cohort}>55-59</option>
-                <option value={newRunner.cohort}>60-64</option>
-                <option value={newRunner.cohort}>70-74</option>
-                <option value={newRunner.cohort}>75-79</option>
-                <option value={newRunner.cohort}>80-84</option>
-                <option value={newRunner.cohort}>85-89</option>
-                <option value={newRunner.cohort}>90+</option>
+                <option value="F18-39">18-39</option>
+                <option value="F40-49">40-49</option>
+                <option value="F50-59">50-59</option>
+                <option value="F60-69">60-69</option>
+                <option value="F70+">70+</option>
+              </optgroup>
+              <opgroup label="Wheelchair">
+                <option value={newRunner.cohort}>Male</option>
+                <option value={newRunner.cohort}>Female</option>
+              </opgroup>
+              <optgroup label="Wheelchair">
+                <option value={newRunner.cohort}>18-39</option>
+                <option value={newRunner.cohort}>40-49</option>
+                <option value={newRunner.cohort}>50-59</option>
+                <option value={newRunner.cohort}>60-69</option>
+                <option value={newRunner.cohort}>70+</option>
+              </optgroup>
+              <optgroup label="Wheelchair Female">
+                <option value={newRunner.cohort}>18-39</option>
+                <option value={newRunner.cohort}>40-49</option>
+                <option value={newRunner.cohort}>50-59</option>
+                <option value={newRunner.cohort}>60-69</option>
+                <option value={newRunner.cohort}>70+</option>
               </optgroup>
             </select>
           </div>
-        </div>
+        </div> */}
 
         <hr></hr>
 
         {/* GOAL TIME NUM INPUT */}
         <div className="container time">
           <span>Goal Time: </span>
+
           <div className="wrapper">
             <label htmlFor="hours">Hours:</label>
             <input
-              id="hours"
+              // id="hours"
               type="number"
+              placeholder="00"
               min="0"
-              max="10"
+              max="99"
               step="1"
-              name="hour"
-              value={newRunner.goal_time}
-              onChange={handleChange}
+              name="hours"
+              // data-type="goal_time"
+              value={goalTime.hours}
+              onChange={handleGoalTimeChange}
             ></input>
           </div>
 
           <div className="wrapper">
             <label htmlFor="minutes">Minutes:</label>
             <input
-              id="minutes"
+              // id="minutes"
               type="number"
+              placeholder="00"
               min="0"
               max="59"
               step="1"
-              name="minute"
-              value={newRunner.goal_time}
-              onChange={handleChange}
+              name="minutes"
+              // data-type="goal_time"
+              value={goalTime.minutes}
+              onChange={handleGoalTimeChange}
             ></input>
           </div>
 
           <div className="wrapper">
             <label htmlFor="seconds">Seconds:</label>
             <input
-              id="seconds"
+              // id="seconds"
               type="number"
+              placeholder="00"
               min="0"
               max="59"
               step="1"
               name="seconds"
-              value={newRunner.goal_time}
-              onChange={handleChange}
+              // data-type="goal_time"
+              value={goalTime.seconds}
+              onChange={handleGoalTimeChange}
             ></input>
           </div>
         </div>
@@ -314,7 +360,8 @@ export default function Register() {
 
         {/* SUBMIT BUTTON */}
         <div className="container">
-          <button type="submit">Submit</button>
+          {/* <button type="submit">Submit</button> */}
+          <input type="submit" />
         </div>
       </form>
     </div>
